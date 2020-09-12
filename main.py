eceb7092ade0f78e150020149e36469692a2a47a
snek <40864710+snekplex@users.noreply.github.com>
2020-09-12 13:50:38 -0500

    Added pyfiglet text

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from pyfiglet import Figlet
 from terminal.app import TradeTermial
 
 if __name__ == '__main__':
+    f = Figlet(font='slant')
+    print(f.renderText('TradeTerminal'))
     app = TradeTermial()
-    app.cmdloop('Welcome to the TradeTerminal. Enter a command into the prompt')
+    app.cmdloop('Welcome to the TradeTerminal. Enter a command into the prompt.')
+    
