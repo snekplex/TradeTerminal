7eb51643ca31296c91158d34f37d5f94c4bb1996
snek <40864710+snekplex@users.noreply.github.com>
2020-09-13 09:52:44 -0500

    Added command descriptions to help command

--- terminal/app.py
+++ terminal/app.py
@@ -5,19 +5,32 @@ class TradeTermial(Cmd):
 
     prompt = '> '
 
+    # Prints the current price of the inputted ticker
     def do_price(self, ticker: str) -> None:
         price = None
         try:
             price = get_ticker_current_price(ticker)
         except AssertionError:
             # Handle if ticker is not found or does not exist
-            print(f'Ticker {ticker} not found')
+            print('Ticker {} not found'.format(ticker))
     
         if price:
             print('{} => ${}'.format(ticker.upper(), price))
 
-    def do_help(self, arg):
-        print('Help here')
+    # Used to find all available commands and their functions
+    def do_help(self, command: str) -> None:
+        commands = {
+            'exit': 'Exits the terminal and closes the application',
+            'price': 'Displays the current price of a ticker'
+        }
 
-    def do_exit(self, args):
+        if not command:
+            for key, val in commands.items():
+                print('{} : {}'.format(key, val))
+
+        if command.lower() in commands:
+            print('{} : {}'.format(command, commands[command]))
+
+
+    def do_exit(self, args) -> None:
         raise SystemExit()
