770be32f1b3808c3e08c54daa752b74c0860c06e
snek <40864710+snekplex@users.noreply.github.com>
2020-09-13 09:41:29 -0500

    Added price command to get ticker price

--- terminal/app.py
+++ terminal/app.py
@@ -1,11 +1,23 @@
 from cmd import Cmd
+from data.yahoo import get_ticker_current_price
 
 class TradeTermial(Cmd):
 
     prompt = '> '
 
-    def do_echo(self, text):
-        print(text)
+    def do_price(self, ticker: str) -> None:
+        price = None
+        try:
+            price = get_ticker_current_price(ticker)
+        except AssertionError:
+            # Handle if ticker is not found or does not exist
+            print(f'Ticker {ticker} not found')
+    
+        if price:
+            print('{} => ${}'.format(ticker.upper(), price))
+
+    def do_help(self, arg):
+        print('Help here')
 
     def do_exit(self, args):
         raise SystemExit()
