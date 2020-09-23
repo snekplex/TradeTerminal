31b4bfd83b907c46c7f13a556bf695b41bd3c7e0
snek <40864710+snekplex@users.noreply.github.com>
2020-09-23 15:00:14 -0500

    Move db Base to __init__.py

--- database/account.py
+++ database/account.py
@@ -1,15 +1,25 @@
 import uuid
 import datetime
 from sqlalchemy import Column, Integer, String, Float, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from database import Base
 
 class Account(Base):
     __tablename__ = 'account'
 
     id = Column(Integer, primary_key=True)
+    uuid =  Column(String(), unique=True, nullable=False)
     name = Column(String(32), unique=True, nullable=False)
+    balance =  Column(Float(), default=100000.00, nullable=False)
 
     def __repr__(self):
         return '<Account: id={} | name={} />'.format(self.id, self.name)
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'balance': self.balance
+        }
+
+
+
