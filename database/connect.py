766e7aaf6079ed8c265d4fd96f44ae81e16dc904
snek <40864710+snekplex@users.noreply.github.com>
2020-09-23 15:02:00 -0500

    Base now create all necessary tables

--- database/connect.py
+++ database/connect.py
@@ -1,14 +1,14 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
+from database import Base
 
 engine = create_engine('sqlite:///app.db')
 
 if not database_exists(engine.url):
     create_database(engine.url)
+    Base.metadata.create_all(engine, checkfirst=True)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 meta = MetaData()
-
-meta.create_all(engine)
