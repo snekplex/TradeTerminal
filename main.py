59a292e3685eb307d0fcee5bbd94c12d58cc8d0d
snek <40864710+snekplex@users.noreply.github.com>
2020-09-25 15:24:07 -0500

    Updated inital startup text

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     f = Figlet(font='slant')
     print(f.renderText('TradeTerminal'))
     app = TradeTermial(completekey='tab', stdin=None, stdout=None)
-    app.cmdloop('Welcome to the TradeTerminal. Enter a command into the prompt.')
+    app.cmdloop('''Welcome to the TradeTerminal. Enter a command into the prompt.\nIf a new user, enter the command newuser to get started''')
     
