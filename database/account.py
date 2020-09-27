abf63e10f995c7cdcd54c5288c29c1fbc34a24cc
snek <40864710+snekplex@users.noreply.github.com>
2020-09-27 14:41:32 -0500

    Added user_id to table

--- database/account.py
+++ database/account.py
@@ -1,12 +1,13 @@
 import uuid
 import datetime
-from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
 from database import Base
 
 class Account(Base):
     __tablename__ = 'account'
 
     id = Column(Integer, primary_key=True)
+    user_id = Column(Integer(), ForeignKey('user.id', ondelete='CASCADE'))
     uuid =  Column(String(), unique=True, nullable=False)
     name = Column(String(32), unique=True, nullable=False)
     balance =  Column(Float(), default=100000.00, nullable=False)
@@ -18,6 +19,7 @@ class Account(Base):
     @property
     def serialize(self):
         return {
+            'user_id': self.user_id,
             'name': self.name,
             'balance': self.balance
         }
